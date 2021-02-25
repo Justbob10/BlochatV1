6d67d10cc84f42a6339b25852445c0e356048016
BuildTools <unconfigured@null.spigotmc.org>
2021-02-25 17:26:10 -0500

    Update visual.py

--- blochat/visual.py
+++ blochat/visual.py
@@ -13,10 +13,10 @@ def pickName():
 root = Tk()
 #https://coolors.co/133c55-386fa4-59a5d8-84d2f6-91e5f6
 root.title('Blochat')
-root.iconbitmap("../CTX/blochat_icon2.ico")
+#root.iconbitmap("../CTX/blochat_icon.ico")
 root.configure(background="#386FA4")
-photo1 = PhotoImage(file="../CTX/blochat_icon2.png")
-Label(root, image=photo1, bg="#386FA4").grid(row=1, column=0, columnspan=2, rowspan=2)
+#photo1 = PhotoImage(file="../CTX/blochat_icon.png")
+#Label(root, image=photo1, bg="#386FA4").grid(row=1, column=0, columnspan=2, rowspan=2)
 Label(root, text="Blochat", bg="#386FA4", fg="white", font="none 100 bold").grid(row=1, column=3, rowspan = 2)
 Label(root, text="", bg="#386FA4", fg="white", font="none 20 bold").grid(row=4, column=0)
 Label(root, text="Server IP:", bg="#386FA4", fg="white", font="none 15 bold").grid(row=5, column=0, sticky=E)
